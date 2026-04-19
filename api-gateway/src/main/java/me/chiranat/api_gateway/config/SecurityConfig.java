061bd10377b9ac8788a1b37c461d5a68cb8e1f82
chiranat <chiranat.pae@gmail.com>
2026-04-19 12:33:11 +0700

    Aggregate REST API in Gateway

--- api-gateway/src/main/java/me/chiranat/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/me/chiranat/api_gateway/config/SecurityConfig.java
@@ -9,15 +9,26 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(Customizer.withDefaults()))
-                .build();
-    }
+        private final String[] freeResourceUrls = {
+                "/swagger-ui.html",
+                "swagger-ui/**",
+                "v3/api-docs/**",
+                "/swagger-resources/**",
+                "/api-docs/**",
+                "/aggregate/**"
+        };
+
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+                return httpSecurity
+                                .csrf(csrf -> csrf.disable())
+                                .authorizeHttpRequests(auth -> auth
+                                                .requestMatchers(freeResourceUrls)
+                                                .permitAll()
+                                                .anyRequest().authenticated())
+                                .oauth2ResourceServer(oauth2 -> oauth2
+                                                .jwt(Customizer.withDefaults()))
+                                .build();
+        }
 
 }
--- api-gateway/src/main/java/me/chiranat/api_gateway/routes/Routes.java
+++ api-gateway/src/main/java/me/chiranat/api_gateway/routes/Routes.java
@@ -1,5 +1,7 @@
 package me.chiranat.api_gateway.routes;
 
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.setPath;
+
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
@@ -19,6 +21,14 @@ public class Routes {
     }
 
     @Bean
+    public RouterFunction<ServerResponse> productServiceSwaggerRoute(){
+        return GatewayRouterFunctions.route("product_service_swagger")
+            .route(RequestPredicates.path("/aggregate/product-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8080"))
+            .before(setPath("/api-docs"))
+            .build();
+    }
+
+    @Bean
     public RouterFunction<ServerResponse> ordertServiceRoute(){
         return GatewayRouterFunctions.route("order_service")
             .route(RequestPredicates.path("/api/orders"), HandlerFunctions.http("http://localhost:8081"))
@@ -26,9 +36,25 @@ public class Routes {
     }
 
     @Bean
+    public RouterFunction<ServerResponse> orderServiceSwaggerRoute(){
+        return GatewayRouterFunctions.route("order_service_swagger")
+            .route(RequestPredicates.path("/aggregate/order-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8081"))
+            .before(setPath("/api-docs"))
+            .build();
+    }
+
+    @Bean
     public RouterFunction<ServerResponse> inventoryServiceRoute(){
         return GatewayRouterFunctions.route("inventory_service")
             .route(RequestPredicates.path("/api/inventory/**"), HandlerFunctions.http("http://localhost:8082"))
+            .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceSwaggerRoute(){
+        return GatewayRouterFunctions.route("inventory_service_swagger")
+            .route(RequestPredicates.path("/aggregate/inventory-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8082"))
+            .before(setPath("/api-docs"))
             .build();
     }
 
